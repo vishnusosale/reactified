64958af375a33db0f949cc78a49b29776e206fd0
Vishnu Sosale <vishnu.s@goniyo.com>
2017-09-28 15:22:27 +0530

    Get Trending GIFs from Giphy

--- index.ios.js
+++ index.ios.js
@@ -2,9 +2,21 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
 import Header from './src/components/Header';
+import axios from 'axios';
 
 // Create a component
 export default class Reactified extends Component {
+
+  componentWillMount() {
+    axios.get('https://api.giphy.com/v1/gifs/trending',
+    {
+      params: {
+        api_key: "AlyTO7N4GWy48wQDTBDW5P4JFy0s7dnT"
+      }
+    })
+    .then(response => console.log(response));
+  }
+
   render() {
     return (
       <Header headerText={"Reactified"}/>
