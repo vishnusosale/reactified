b491f6cb02b047d06fe34c51926d14e35af1d8c8
Vishnu Sosale <vishnu.s@goniyo.com>
2017-10-01 02:03:28 +0530

    Update for eslint lint errors

--- index.ios.js
+++ index.ios.js
@@ -1,26 +1,23 @@
 // Import a library to help create a component
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import Header from './src/components/Header';
 import axios from 'axios';
+import Header from './src/components/Header';
 
 // Create a component
 export default class Reactified extends Component {
-
   componentWillMount() {
-    axios.get('https://api.giphy.com/v1/gifs/trending',
-    {
-      params: {
-        api_key: "AlyTO7N4GWy48wQDTBDW5P4JFy0s7dnT"
-      }
-    })
-    .then(response => console.log(response));
+    axios
+      .get('https://api.giphy.com/v1/gifs/trending', {
+        params: {
+          api_key: 'AlyTO7N4GWy48wQDTBDW5P4JFy0s7dnT',
+        },
+      })
+      .then(response => console.log(response));
   }
 
   render() {
-    return (
-      <Header headerText={"Reactified"}/>
-    );
+    return <Header headerText={'Reactified'} />;
   }
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,25 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Text, View } from 'react-native';
 
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+  },
+  textStyle: {
+    fontSize: 20,
+  },
+};
 
 export default class Header extends Component {
   render() {
+    const { viewStyle, textStyle } = styles;
     return (
-      <View style={styles.viewStyle}>
-        <Text style={styles.textStyle}>{this.props.headerText}</Text>
+      <View style={viewStyle}>
+        <Text style={textStyle}>{this.props.headerText}</Text>
       </View>
     );
   }
 }
+
 Header.propTypes = {
   headerText: PropTypes.string.isRequired,
-};
-
-const styles = {
-  viewStyle: {
-    backgroundColor: '#F8F8F8'
-  },
-  textStyle: {
-    fontSize: 20
-  }
 };
